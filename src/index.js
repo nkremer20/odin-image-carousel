38a0c65950b75e09123ce1a3832a487b48e25a67
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-09-21 01:08:41 -0500

    Added the ability to change the visible image using the next and previous buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles.css';
+
+let imageContainer = document.querySelector('.images');
+const nextBtn = document.getElementById('next');
+const previousBtn = document.getElementById('previous');
+
+nextBtn.addEventListener('click', () => {
+    const image = imageContainer.firstElementChild;
+
+    imageContainer.appendChild(image);
+})
+
+previousBtn.addEventListener('click', () => {
+    const image = imageContainer.lastElementChild;
+
+    imageContainer.insertBefore(image, imageContainer.firstElementChild);
+})
