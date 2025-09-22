858b7e07b6655d6ecc85b0b62001430cd3629f63
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-09-21 22:03:11 -0500

    Added image change timer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import './styles.css';
 
-let imageContainer = document.querySelector('.images');
+const imageContainer = document.querySelector('.images');
 const nextBtn = document.getElementById('next');
 const previousBtn = document.getElementById('previous');
 
-nextBtn.addEventListener('click', () => {
+function nextImage() {
     const image = imageContainer.firstElementChild;
-
     imageContainer.appendChild(image);
+    console.log('next image')
+}
+
+function previousImage() {
+    const image = imageContainer.lastElementChild;
+    imageContainer.insertBefore(image, imageContainer.firstElementChild);
+    console.log('previous image')
+}
+
+// Initialize timer variable
+let timer;
+
+// Change image every 3 seconds
+function setTimer() {
+    clearInterval(timer);
+
+    timer = setInterval(nextImage, 3000);
+}
+
+// Start image change timer when the page loads
+setTimer();
+
+nextBtn.addEventListener('click', () => {
+    nextImage();
+    setTimer();
 })
 
 previousBtn.addEventListener('click', () => {
-    const image = imageContainer.lastElementChild;
-
-    imageContainer.insertBefore(image, imageContainer.firstElementChild);
+    previousImage();
+    setTimer();
 })
